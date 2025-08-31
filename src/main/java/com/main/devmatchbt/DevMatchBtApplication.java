b0882029fe2865ae1cfc204807416143ab52a35c
KimYoonMin <dbsalszz@naver.com>
2025-08-31 21:11:53 +0900

    DB 연결 초기 설정

--- src/main/java/com/main/devmatchbt/DevMatchBtApplication.java
+++ src/main/java/com/main/devmatchbt/DevMatchBtApplication.java
@@ -1,8 +1,10 @@
 package com.main.devmatchbt;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing //배치 기능 활성
 @SpringBootApplication
 public class DevMatchBtApplication {
 
