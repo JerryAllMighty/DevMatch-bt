03a170dcea328f02566afb27352662658c1b9ddf
KimYoonMin <dbsalszz@naver.com>
2025-08-31 20:42:28 +0900

    배치 초기 세팅

---
+++ src/main/java/com/main/devmatchbt/DevMatchBtApplication.java
@@ -0,0 +1,13 @@
+package com.main.devmatchbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMatchBtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevMatchBtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/main/devmatchbt/DevMatchBtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.devmatchbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMatchBtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
